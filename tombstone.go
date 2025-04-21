2a0da72cf92ede7eddb259b15e0533c4fe4c6434
Daenney <daenney@noreply.codeberg.org>
2025-04-21 13:55:48 +0200

    Complete Tombstone

--- tombstone.go
+++ tombstone.go
@@ -11,7 +11,7 @@ type Tombstone Object
 // NewTombstone initialises a new Tombstone.
 func NewTombstone() *Tombstone {
 	return &Tombstone{
-		Properties: make(ld.Properties),
+		Properties: make(ld.Properties, 1),
 		Type:       []string{as.TypeTombstone},
 	}
 }
@@ -19,4 +19,41 @@ func NewTombstone() *Tombstone {
 // Build finalises the Tombstone.
 func (t *Tombstone) Build() Tombstone {
 	return *t
+}
+
+// See [Object.GetID].
+func (t *Tombstone) GetID() string {
+	return (*Object)(t).GetID()
+}
+
+// See [Object.SetID].
+func (t *Tombstone) SetID(id string) *Tombstone {
+	(*Object)(t).SetID(id)
+	return t
+}
+
+// See [Object.GetType].
+func (t *Tombstone) GetType() string {
+	return (*Object)(t).GetType()
+}
+
+// See [Object.SetType].
+func (t *Tombstone) SetType(id string) *Tombstone {
+	(*Object)(t).SetType(id)
+	return t
+}
+
+// Returns the type in [as.FormerType].
+func (t *Tombstone) GetFormerType() string {
+	if nodes := (*ld.Node)(t).GetNodes(as.FormerType); len(nodes) != 0 {
+		return nodes[0].ID
+	}
+
+	return ""
+}
+
+// Sets the type in [as.FormerType].
+func (t *Tombstone) SetFormerType(id string) *Tombstone {
+	(*ld.Node)(t).SetNodes(as.FormerType, ld.Node{ID: id})
+	return t
 }
