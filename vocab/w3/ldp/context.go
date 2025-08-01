ccb911b7ccd41d4ba5f02037a3106c79c686e88f
Daenney <git@noreply.sourcery.dny.nu>
2025-08-01 18:09:35 +0200

    Fix the Namespace for LDP

--- vocab/w3/ldp/context.go
+++ vocab/w3/ldp/context.go
@@ -2,7 +2,7 @@
 package ldp
 
 // Namespace is the IRI prefix used for terms defined in this namespace.
-const Namespace = "https://www.w3.org/ns/ldp#"
+const Namespace = "http://www.w3.org/ns/ldp#"
 
 const (
 	// Inbox is an IRI, either as a string or as an object with an id property.
